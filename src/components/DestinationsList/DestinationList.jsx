fc11ed304a32f0542c16be1e79ff979784b59e7b
Maria Nogales <maria.nogales99@gmail.com>
2024-03-07 15:49:38 +0100

    routes to DestinationsPage + All Destinations added to NavBar

---
+++ src/components/DestinationsList/DestinationList.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import DestinationCard from "../DestinationCard/DestinationCard"
+
+
+const DestinationsList = () => {
+
+    return (
+        <div>
+            <h1>DESTINATIONS LIST</h1>
+            <DestinationCard></DestinationCard>
+        </div>
+    )
+}
+
+export default DestinationsList
--- src/pages/DestinationsPage/DestinationsPage.jsx
+++ src/pages/DestinationsPage/DestinationsPage.jsx
@@ -1,12 +1,20 @@
 import React from "react"
 
+import DestinationsList from "../../components/DestinationsList/DestinationList"
+import { Container, Row, Col } from "react-bootstrap"
 
 const DestinationsPage = () => {
 
     return (
 
-        <h1>hola</h1>
-
+        <Container>
+            <Row>
+                <Col>
+                    <h1>hola</h1>
+                    <DestinationsList></DestinationsList>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
