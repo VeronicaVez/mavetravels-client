9071e8083aaf67aaeff4cef66378ea4d8fc498c5
Maria Nogales <maria.nogales99@gmail.com>
2024-03-07 20:11:27 +0100

    trying to fix services

---
+++ src/pages/TravelDetailsPage/TravelDetailsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+
+const TravelDetailsPage = () => {
+
+    return (
+        <Container>
+            <h1>Travel Details</h1>
+        </Container>
+    )
+}
+
+export default TravelDetailsPage
