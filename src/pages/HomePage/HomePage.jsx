7c687e0e2707e9a8280c24c0794f5accd709a8a6
Maria Nogales <maria.nogales99@gmail.com>
2024-03-13 16:13:06 +0100

    HomePage styles done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,30 +1,33 @@
 import React from "react"
+import { Container, Col, Row } from "react-bootstrap"
+
 import Cover from "./../../images/Turkey.jpg"
-import NewTravelForm from "./../../components/Forms/NewTravelForm/NewTravelForm.jsx"
 import Morocco from "./../../images/Morocco.jpg"
 import Namibia from "./../../images/Namibia.jpg"
 import Thailand from "./../../images/Thailand.jpg"
 import Brazil from "./../../images/Brazil.jpg"
 import ReviewsCarousel from "./../../components/ReviewsCarousel/ReviewsCarousel.jsx"
+
 import "./HomePage.css"
-import {Container, Col, Row} from "react-bootstrap"
 
 const HomePage = () => {
 
     return (
 
         <div className="HomePage">
+
             <img src={Cover} alt="Capadocia" className="coverImage" />
+
             <Container>
                 <Col>
                     <Row className="trips">
-                        <h2>Our Trips</h2>
+                        <h2 className="ourTripTitle">Our Trips</h2>
                         <Col>
                             <Row>
                                 <div>
                                     <img src={Thailand} alt="Thailand" className="Thailand" />
                                     <h3 className="favTrips">Here our top 3 destinations</h3>
-                                    <p className="paragr">su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas v</p>
+                                    <p className="favTripsParagraf">Embark on an unforgettable adventure to three extraordinary destinations: Namibia's surreal landscapes, Thailand's vibrant culture, and Brazil's pulsating rhythms. Discover the awe-inspiring beauty of Namibia's deserts, immerse yourself in Thailand's bustling markets, and feel the rhythm of Brazil's vibrant streets. Whether you crave adventure, relaxation, or cultural exploration, these destinations offer experiences that will leave you enchanted. Join us on a journey to Namibia, Thailand, and Brazil, and let your wanderlust take flight.</p>
                                 </div>
                                 <div>
                                     <img src={Namibia} alt="Namibia" className="Namibia" />
@@ -48,7 +51,8 @@ const HomePage = () => {
                             <Row>
                                 <p className="phraseonly">
                                     “We live in a wonderful world that is full of beauty, charm and adventure. There is no end to the adventures we can have if only we seek them with our eyes open.”
-                                </p><p className="authoronly">
+                                </p>
+                                <p className="authoronly">
                                     Jawaharial Nehru
                                 </p>
                             </Row>
@@ -57,9 +61,6 @@ const HomePage = () => {
                     </Row>
                 </Col>
             </Container>
-            <footer>
-                <p>Developed by Maria and Veronica</p>
-            </footer>
         </div>
 
     )
